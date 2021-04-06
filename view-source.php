ea850d7e4f25860c7d3901112a6ef25026b7d79c
Zero <liambocakra@gmail.com>
2021-04-06 08:19:28 +0700

    Update view-source.php

--- view-source.php
+++ view-source.php
@@ -1,19 +1,42 @@
 <html>
-<head><title>View Source URL or Domain</title>
+<head>
+<title>View Source URL</title>
+<link rel="stylesheet" href="//markdownmonster.west-wind.com/docs/templates/scripts/highlightjs/styles/vs2015.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.11.0/highlight.min.js"></script>
 </head>
 <body>
+<style>
+body {
+	background-color:#212121;
+	color: white;
+}
+
+.result {
+    display: none;
+    white-space: pre-wrap;
+    word-break: break-word;
+    width: 100%;
+    margin: 10px auto;
+    padding: 0px 10px 10px 20px;
+    font-size: 13px;
+    overflow-y: scroll;
+    border: 1px solid #d6d6d6;
+    background: #151313;
+    height: 100%;
+}
+</style>
 <table width="100%" height="100%">
 <td align="center">
-<font size='7'>
-<a href='//anoaghost.github.io' style='text-decoration: none; text-shadow: 2px 0px 4px #ff0000, 3px 0px 5px #ff0000, 0px 3px 4px #ff0000; font-weight: bold; color: black;'>View Source URL by AnoaGhost</a>
-</font><br>
+<center>
+<font face='Iceland' size='7'><a href='javascript:window.assign("//anoaghost.github.io")' style='text-decoration: none; text-shadow: 2px 0px 4px black, 3px 0px 5px black, 0px 3px 4px black; font-weight: bold;color:red;'>View Source Website or URL</a></font><br>
 <form name="data" method="post">
-<input type="text" name="host" placeholder="URL" required></input><br>
+<label for="host">URL:</label><br><input type="text" name="host" placeholder="URL" required></input><br>
 <input type="submit" name='submit' value="Submit"><br>
 </form>
+</center>
 <?php
-	
 $url = htmlspecialchars($_POST['host']);
+$parseurl = parse_url($url)['host'];
 if($_POST['submit']){
 	$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -22,10 +45,18 @@ if($_POST['submit']){
 		curl_close($ch);
 	$result = htmlspecialchars($content);
 		
-	echo "<textarea class=\"result\" style=\"width: 650px; height: 350px !important; white-space: normal; margin: 20px auto 0px; padding: 7px; text-align: left !important; display: block;\">".$result."\n</textarea>";
+	echo "<center><b>URL : ".$url."<br>\n IP : ".gethostbyname($parseurl)."</b></center><br><pre class=\"result\" style=\"width: 950px; height: 450px !important; margin: 20px auto 0px; padding: 7px; text-align: left !important; display: block;\"><code class=\"language-html\">".$result."\n</code></pre>";
 }
-		
 ?>
+<script>
+function highlightCode() {
+    var pres = document.querySelectorAll("pre>code");
+    for (var i = 0; i < pres.length; i++) {
+        hljs.highlightBlock(pres[i]);
+    }
+}
+highlightCode();
+</script>
 </td>
 </table>
 </body>
